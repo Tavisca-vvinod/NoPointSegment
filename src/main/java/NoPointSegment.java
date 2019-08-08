c82d9e5f31154494f53df088ab8d1ddc0364f1e6
vvinod <vvinod@tavisca.in>
2019-08-08 14:16:09 +0530

    Wrote function to generate all points on a line segment.

--- src/main/java/NoPointSegment.java
+++ src/main/java/NoPointSegment.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-
 public class NoPointSegment {
     public static String findIntersection(int[] line1Points, int[] line2Points) {
         LineSegment line1 = new LineSegment(line1Points[0],line1Points[1],line1Points[2],line1Points[3]);
@@ -10,6 +9,20 @@ public class NoPointSegment {
     }
     public static ArrayList<Integer[]> findPointsOnLine(LineSegment line)
     {
-        return null;
+        if(line.parallelTo.equals("x"))
+        {
+            for(int i=line.start.y;i<=line.end.y;i++)
+            {
+                line.Points.add(new Integer[]{line.start.x,i});
+            }
+        }
+        if(line.parallelTo.equals("y"))
+        {
+            for(int i=line.start.x;i<=line.end.x;i++)
+            {
+                line.Points.add(new Integer[]{i,line.start.y});
+            }
+        }
+        return line.Points;
     }
 }
